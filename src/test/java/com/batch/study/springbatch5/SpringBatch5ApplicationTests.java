4ece0140e19a4d88d9d9ca845c77e31c6e256704
mugeon <pos04167@kakao.com>
2024-10-15 20:59:42 +0900

    init project

---
+++ src/test/java/com/batch/study/springbatch5/SpringBatch5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.study.springbatch5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
